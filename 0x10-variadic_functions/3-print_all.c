3b4ecdacff2b441c8db83f80e546c02160d168b4
alexditah <alexditah@gmail.com>
2022-04-14 20:56:50 +0100

    3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,54 +1,102 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdarg.h>
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
- * print_all - prints anything
- * @format: any variable type
- * Return: Nothing
-**/
+ * print_all - function that prints anything
+ * const char * const format = constant pointer to a constant variable
+ * @format: variable, list of types of arguments passed to the function
+ *
+ * Return: void
+ */
 
 void print_all(const char * const format, ...)
 {
-unsigned int i = 0, j = 0;
-char *str;
-va_list ls;
-va_start(ls, format);
+	va_list args;
+	unsigned int i, j;
+	char *ptr = "";
 
-while (format && format[i])
-{
-switch (format[i])
-{
-case 'c':
-printf("%c", va_arg(ls, int));
-break;
-case 'i':
-printf("%i", va_arg(ls, int));
-break;
-case 'f':
-printf("%f", va_arg(ls, double));
-break;
-case 's':
-str = va_arg(ls, char *);
-if (str == NULL)
-str = "(nil)";
+	var_t type[] = {
+		{"c", c_char},
+		{"i", i_integer},
+		{"f", f_float},
+		{"s", s_string},
+		{NULL, NULL}
+	};
 
-printf("%s", str);
-break;
-}
-j = i + 1;
-
-while (format[j] && (format[j] == 'c' || format[j] == 'i' ||
-format[j] == 'f' || format[j] == 's'))
-{
-printf(", ");
-break;
+	va_start(args, format);
+	i = 0;
+	while (format && format[i])
+	{
+		j = 0;
+		while (type[j].vartype)
+		{
+			if (format[i] == *type[j].vartype)
+			{
+				printf("%s", ptr);
+				type[j].f(args);
+				ptr = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
 }
 
-i++;
+/**
+ * c_char - prints a char
+ * @args: parameter passed from main
+ *
+ * Return: void
+ */
+
+void c_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
 }
-printf("\n");
-va_end(ls);
-return;
+
+/**
+ * i_integer - prints an integer
+ * @args: parameter passed from main
+ *
+ * Return: void
+ */
+
+void i_integer(va_list args)
+{
+	printf("%i", va_arg(args, int));
+}
+
+/**
+ * f_float - prints a float
+ * @args: parameter passed from main
+ *
+ * Return: void
+ */
+
+void f_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+ * s_string - prints a string
+ * @args: parameter passed from main
+ *
+ * Return: void
+ */
+
+void s_string(va_list args)
+{
+	char *str;
+
+	str = va_arg(args, char *);
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", str);
 }
